ad19a0123e301f0d3f0add40b1c2a1c888752a18
cantstopcoding <jonathan_koss@yahoo.com>
2022-02-15 21:05:06 -0500

    fix typo

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'User visits homepage' do
   scenario 'successfully' do
-    vistit root_path
+    visit root_path
 
     expect(page).to have_css 'h1', text: 'Counter'
   end
